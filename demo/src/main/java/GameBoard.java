6ae9f0486740dc347b04e5030a999194e50b27f2
Xokto <xoktonix@krogalf.com>
2022-05-18 15:22:18 +0200

    Création de la classe GameBoard et Hand + commentaires et bouchon

--- demo/src/main/java/GameBoard.java
+++ demo/src/main/java/GameBoard.java
@@ -1,2 +1,48 @@
+/*
+ * GameBoard.java                                                   18/05/2022
+ * IUT Rodez, INFO 1, SAE Hanabi
+ */
+
+package example;
+
+
+/**
+ * TODO le commentaire
+ *
+ */
+
 public class GameBoard {
+
+    private Card[] board = {
+            new Card("white", 0),
+            new Card("yellow", 0),
+            new Card("blue", 0),
+            new Card("pink", 0),
+            new Card("red", 0)
+    };
+
+    /**
+     * Plateau de jeu où les cartes seront placées quand elles seront jouées
+     */
+    private Card[] gameBoard {
+
+
+        return this; // gros bouchon
+    }
+
+
+    /**
+     * ajoute une carte sur le plateau si elle est correcte
+     * @param toAdd  la carte à ajouter
+     * @return  la carte à ajouter
+     * @throws IllegalArgumentException si la carte à ajouter a une valeur
+     *                                 inférieure ou supérieure
+     *                                 à la valeur attendue
+     */
+    public Card addCard(Card toAdd) {
+
+
+        return toAdd;
+    }
+
 }
