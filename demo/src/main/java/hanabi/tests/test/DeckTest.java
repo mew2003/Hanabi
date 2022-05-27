203d64342a546f346ed3701a3c6ef45db431abe8
mew2003 <mewen.derruau@gmail.com>
2022-05-27 21:20:08 +0200

    JUNIT C'EST DE LA GROSSE MERDE

---
+++ demo/src/main/java/hanabi/tests/test/DeckTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import principal.Card;
+import principal.Deck;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeckTest extends Deck {
+
+    @Test
+    void getCardsTest() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void getNbCardsTest() {
+        fail("Not yet implemented");
+    }
+}
