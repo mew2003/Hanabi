02fcfcc296a5a0f1a57a9c37b0338d46ac9b95e3
mew2003 <mewen.derruau@gmail.com>
2022-05-28 23:38:23 +0200

    WAW trop beau la vie

--- src/application/MainScene.java
+++ src/application/MainScene.java
@@ -5,15 +5,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 
 public class MainScene extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try { 
-			Parent root = FXMLLoader.load(getClass().getResource("/leTest.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("leTest.fxml"));
 			Scene scene = new Scene(root,1920,1080);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("HANABI");
+			Image icon = new Image("file:../../resources/icon/icone-fleur-couleur.png");
+			primaryStage.getIcons().add(icon);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
