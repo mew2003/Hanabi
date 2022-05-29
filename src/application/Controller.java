bedb4615a9db995e750473b365045f416d56f197
mew2003 <mewen.derruau@gmail.com>
2022-05-29 19:04:31 +0200

    Lancement d'un partie (Interface)

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -8,13 +8,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Controller {
-
+	
+	@FXML
+    private Pane escapeMenu;
 
     public void initialize() {
-        // TODO
+    	
     }
     
     public Stage stage;
@@ -50,6 +55,33 @@ public class Controller {
 		stage.setScene(scene);
 		stage.show();
     } 
+    
+    @FXML
+    protected void switchToGameBoard2(ActionEvent e) throws IOException {
+    	root = FXMLLoader.load(getClass().getResource("GameBoard2.fxml"));
+		stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("GameBoard.css").toExternalForm());
+		stage.setScene(scene);
+		stage.show();
+    }
+    
+    @FXML
+    protected void pressed(KeyEvent e) throws IOException {
+    	if (e.getCode() == KeyCode.ESCAPE) {
+    		if (escapeMenu.isVisible()) {
+    			escapeMenu.setVisible(false);
+    		} else {
+    			escapeMenu.setVisible(true);
+    		}
+    	}
+    }
+    
+    @FXML
+    protected void returnGame(ActionEvent e) throws IOException {
+    	System.out.println("test");
+    	escapeMenu.setVisible(false);
+    }
     
 
 }
